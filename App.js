273b2ee7f9f61d26663a6b57be7ed54615d5502e
r li <r.li@mail.com>
2024-12-31 21:25:21 +0000

    10.3 work in progress

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RepositoryList from './src/RepositoryList';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <RepositoryList />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const HelloWorld = props =>{
+  return <Text>Hello World!</Text>
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
