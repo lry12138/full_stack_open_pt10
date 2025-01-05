945d42db41ef33e530b0c08ac163f7b691e6258e
r li <r.li@mail.com>
2025-01-05 14:10:53 +0000

    10.8

--- components/Main.jsx
+++ components/Main.jsx
@@ -7,13 +7,13 @@ import SignIn from './SignIn';
 
 const Main = () => {
   return (
-    <View style={styles.flexContainer}>
+    <View style={styles.contents}>
       <View style={styles.headerBar}> 
         
         <AppBar />
         <Routes>
-            <Route path="/repo" element={<View style ={styles.contents}><RepositoryList/></View>} />
-            <Route path="/signin" element={<View style ={styles.contents}><SignIn /></View>} />
+            <Route path="/repo" element={<RepositoryList/>} />
+            <Route path="/signin" element={<SignIn />} />
             <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </View>
@@ -22,9 +22,6 @@ const Main = () => {
 }
 
 const styles = StyleSheet.create({
-  flexContainer: {
-    display: 'flex',
-  },
   headerBar: {
     flexGrow: 1,
     
--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -1,7 +1,72 @@
 import Text from './Text';
+import {TextInput, Pressable, View ,StyleSheet} from 'react-native';
+import { useFormik } from 'formik';
+import theme from '../src/theme';
 
-const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+const initialValues ={
+  username:"",
+  password:"",
 };
+
+const SignInForm = ({onSubmit}) => {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  })
+  return (
+    <View style={styles.container}>
+      <TextInput
+          value = {formik.values.username}
+          style= {styles.input}
+          placeholder = "Username"
+          onChangeText={formik.handleChange('username')}
+        />
+        <TextInput
+          value = {formik.values.password}
+          style= {styles.input}
+          placeholder = "Password"
+          secureTextEntry
+          onChangeText={formik.handleChange('password')}
+        />
+        <Pressable style= {styles.button} onPress = {formik.handleSubmit}>
+          <Text color='onDark'>Submit</Text>
+        </Pressable>
+      
+    </View>
+  );
+};
+
+const SignIn = () =>{
+  const onSubmit = values =>{
+    console.log(values)
+  };
+
+  return (
+    <SignInForm onSubmit={onSubmit} />
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:'#fff',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderColor: theme.colors.backgroundMain,
+    padding: 10,
+    borderRadius:10,
+    
+  },
+  button:{
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderRadius:10,
+    backgroundColor: theme.colors.primary,
+    alignItems:"center",
+  }
+});
 
 export default SignIn;
